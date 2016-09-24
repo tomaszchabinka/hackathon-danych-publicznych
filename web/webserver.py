d2f947ffd17a73ec266eec03f1ebb4a336c7794d
rosz <anna.roszman@gmail.com>
2016-09-24 18:06:45 +0200

    all templates edited, css styling done

--- web/webserver.py
+++ web/webserver.py
@@ -7,9 +7,28 @@ app.config.from_object(__name__)
 
 
 @app.route('/')
-def input_form():
+def rendering_main_page():
     return render_template("index.html")
 
+
+@app.route('/link')
+def input_link_form():
+    return render_template("input_link.html")
+
+
+@app.route('/text')
+def input_text_form():
+    return render_template("input_text.html")
+
+
+@app.route('/pdf')
+def input_pdf_form():
+    return render_template("input_pdf.html")
+
+
+@app.route('/img')
+def input_img_form():
+    return render_template("input_img.html")
 # @app.route('/', methods=['POST'])
 # def my_form_post():
 #     text = request.form['text']
