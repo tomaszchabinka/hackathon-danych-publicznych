c1f14e01927fbc7bf0c85ce0cd3990d38f5b3f0f
rosz <anna.roszman@gmail.com>
2016-09-24 12:57:48 +0200

    templates dir created - one template, index.html; webserver.py file edited - render_template funtion done

---
+++ web/webserver.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def input_form():
+    return render_template("input_text.html")
+
+# @app.route('/', methods=['POST'])
+# def my_form_post():
+#     text = request.form['text']
+#     processed_text = text.upper()
+#     return processed_text
+
+if __name__ == "__main__":
+    app.run()
